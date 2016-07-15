942430296346c59cedc6f5a356010839562e5f2d
Hai Leader <Hai Leader>
2016-07-15 19:19:13 +0700

    * edit navigation drawer

--- app/src/main/java/vn/brine/haileader/expolatorysearch/MainActivity.java
+++ app/src/main/java/vn/brine/haileader/expolatorysearch/MainActivity.java
@@ -1,17 +1,18 @@
 package vn.brine.haileader.expolatorysearch;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import vn.brine.haileader.expolatorysearch.fragments.FavoriteFragment;
 import vn.brine.haileader.expolatorysearch.fragments.HomeFragment;
@@ -22,11 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private NavigationView navigationView;
-
-    private static final int HOME_FRAGMENT = 0;
-    private static final int WHATS_HOT = 1;
-    private static final int FAVORITE_FRAGMENT = 2;
-    private static final int SETTING_FRAGMENT = 3;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,48 +31,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        navigationView = (NavigationView)findViewById(R.id.navigation_view);
-        initNavigationDrawer();
 
-        displayView(HOME_FRAGMENT);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        actionBarDrawerToggle = setupDrawerToggle();
+
+        setupDrawerContent(navigationView);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        if (getFragmentManager().findFragmentById(R.id.container_body) == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_body, new HomeFragment()).commit();
+        }
     }
 
-    public void initNavigationDrawer() {
+    public void setupDrawerContent(NavigationView navigationView ) {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                int id = menuItem.getItemId();
-
-                switch (id){
-                    case R.id.home:
-                        displayView(HOME_FRAGMENT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.whatshot:
-                        displayView(WHATS_HOT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.favorites:
-                        displayView(FAVORITE_FRAGMENT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.settings:
-                        displayView(SETTING_FRAGMENT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.logout:
-                        finish();
-
-                }
+                selectDrawerItem(menuItem);
                 return true;
             }
         });
-//        View header = navigationView.getHeaderView(0);
-//        TextView tv_email = (TextView)header.findViewById(R.id.tv_email);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+    }
 
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+    private void selectDrawerItem(MenuItem menuItem){
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                fragmentClass = HomeFragment.class;
+                break;
+            case R.id.whatshot:
+                fragmentClass = NewFragment.class;
+                break;
+            case R.id.favorites:
+                fragmentClass = FavoriteFragment.class;
+                break;
+            case R.id.settings:
+                fragmentClass = SettingFragment.class;
+                break;
+            case R.id.logout:
+                finish();
+                break;
+            default:
+                fragmentClass = HomeFragment.class;
+        }
+        try {
+            assert fragmentClass != null;
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container_body, fragment).commit();
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+
+        drawerLayout.closeDrawers();
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
             @Override
             public void onDrawerClosed(View v){
                 super.onDrawerClosed(v);
@@ -85,48 +103,40 @@ public class MainActivity extends AppCompatActivity {
                 super.onDrawerOpened(v);
             }
         };
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
         actionBarDrawerToggle.syncState();
     }
 
-    private void displayView(int position){
-        Fragment fragment = null;
-        Class fragmentClass = null;
-        String title = getString(R.string.app_name);
-        switch (position) {
-            case HOME_FRAGMENT:
-                fragmentClass = HomeFragment.class;
-                title = getString(R.string.title_home);
-                break;
-            case WHATS_HOT:
-                fragmentClass = NewFragment.class;
-                title = getString(R.string.whats_hot);
-                break;
-            case FAVORITE_FRAGMENT:
-                fragmentClass = FavoriteFragment.class;
-                title = getString(R.string.title_favorites);
-                break;
-            case SETTING_FRAGMENT:
-                fragmentClass = SettingFragment.class;
-                title = getString(R.string.title_settings);
-                break;
-            default:
-                break;
-        }
-
-        try {
-            fragment = (Fragment) fragmentClass.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (fragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.container_body, fragment);
-            fragmentTransaction.commit();
-            getSupportActionBar().setTitle(title);
-        }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
     }
-
 }
